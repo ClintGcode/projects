a6707e252d849297bed8d3122d6dde769d8f03e2
Clinton <clintongarwood@gmail.com>
2021-12-07 21:13:30 -0500

    add ravenous yelp clone

---
+++ clones/ravenous/src/util/Yelp.js
@@ -0,0 +1,37 @@
+const apiKey =
+  "W4IjmLehBftlnlVP9EnWepXkDZXTH8WlHjwrO7ZUr-FwyzwYjK-9Z5Ljj7axPvLU-8ZSC9SqKEJKuqQWhHKi_CWcYWlehKiWxTQANSKenvhqOPGio-wz6S1abDo3YXYx";
+
+const Yelp = {
+  searchYelp(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => {
+            console.log(business);
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
+  },
+};
+
+export default Yelp;
