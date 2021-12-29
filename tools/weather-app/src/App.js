f37827e63a1d587d96429f84b7e6d78cb991dd9c
Clinton <clintongarwood@gmail.com>
2021-12-29 17:29:55 -0500

    WIP: modify navbar

--- tools/weather-app/src/App.js
+++ tools/weather-app/src/App.js
@@ -1,84 +1,119 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // grab api key and base url from openweathermap.org
 const api = {
-  key: '5484c320271521c57a7d43521a271d85',
-  base: 'http://api.openweathermap.org/data/2.5/'
-}
+  key: "5484c320271521c57a7d43521a271d85",
+  base: "http://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
-  const search = e => {
+  const search = (e) => {
     if (e.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
+        .then((res) => res.json())
+        .then((result) => {
           setWeather(result);
-          setQuery('');
+          setQuery("");
           console.log(result);
         });
     }
-  }
-
+  };
 
   const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${month} ${date} ${year}`
-  }
+    return `${day} ${month} ${date} ${year}`;
+  };
 
   return (
-    <div className={(typeof weather.main !== "undefined") ? ((weather.main.temp > 50) ? 'App warm' : 'App') : 'App'}>
+    <div
+      className={
+        typeof weather.main !== "undefined"
+          ? weather.main.temp > 50
+            ? "App warm"
+            : "App"
+          : "App"
+      }
+    >
       {/*Navbar*/}
       <nav class="nav">
         <ul id="directory" className="nav">
-          <li><a href="default.asp">Weather</a></li>
-          <li><a href="news.asp">News</a></li>
-          <li><a href="contact.asp">Traffic</a></li>
-          <li><a href="about.asp">About</a></li>
+          <li>
+            <a href="default.asp">Weather</a>
+          </li>
+          <li>
+            <a href="news.asp">News</a>
+          </li>
+          <li>
+            <a href="contact.asp">Traffic</a>
+          </li>
+          <li>
+            <a href="about.asp">About</a>
+          </li>
         </ul>
+        <div id="logo">
+          <h1>WTHR</h1>
+        </div>
       </nav>
       <main>
-
         {/* Todays Weather */}
         <div className="search-box">
-          <input 
+          <input
             type="text"
             className="search-bar"
             placeholder="Enter a city..."
-            onChange={e => setQuery(e.target.value)}
+            onChange={(e) => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
-        {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°F
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°F</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
           </div>
-        </div>
         ) : (
           <div className="weather-box">
-          <div className="temp">
-            WTHR
+            <div className="temp">WTHR</div>
           </div>
-          
-        </div>
         )}
       </main>
     </div>
